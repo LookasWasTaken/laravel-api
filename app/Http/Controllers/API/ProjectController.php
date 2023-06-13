162d383f298e38bf03f997d4fe5968c1c40d8466
LookasWasTaken~ <piacen.luca@gmail.com>
2023-06-13 14:07:08 +0200

    ADD API

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(){
+
+        $projects = Project::with(["technologies", "type"])->orderBy("id")->get();
+        return response()->json([
+            'success' => true,
+            'projects' => $projects,
+        ]);
+    }
 }
